6723e2e588305ff94c1ef3b4c47996f90313a35b
palmeiroresearch <palmeiro.research@gmail.com>
2025-09-07 13:44:59 -0400

    Create service-worker.js
    Add service worker for offline functionality

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'dosis-pediatricas-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './medications.json',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
